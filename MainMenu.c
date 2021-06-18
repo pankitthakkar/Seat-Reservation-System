f9f3bdfa72c55538ca1fb8bafe408db569b1bf21
Pankit Thakkar <pankitthakkar007@gmail.com>
2021-06-17 20:27:31 -0400

    Created MainMenu.c

---
+++ MainMenu.c
@@ -0,0 +1,12 @@
+#include"main.h"
+
+void MainMenu(void) // This function will output the menu with the text (options for the users) written below.
+{
+    puts("Choose your flight:");
+    printf("\n");
+    puts("a) Flight 102");
+    puts("b) Flight 311");
+    puts("c) Flight 444");
+    puts("d) Flight 519");
+    puts("e) Quit");
+}
